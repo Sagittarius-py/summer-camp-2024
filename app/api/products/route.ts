f6706ca30f5dc6b9be11ebd340610355dbede962
Filip <sieniu212@gmail.com>
2024-06-24 16:24:13 +0200

    Changes to be committed: 	new file:   .eslintrc.json 	new file:   .gitignore 	new file:   .prettierrc 	new file:   CONTRIBUTING.md 	new file:   README.md 	new file:   app/api/products/route.ts 	new file:   app/favicon.ico 	new file:   app/globals.css 	new file:   app/layout.tsx 	new file:   app/page.tsx 	new file:   app/products/page.tsx 	new file:   lib/products.test.ts 	new file:   lib/products.ts 	new file:   next.config.mjs 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.mjs 	new file:   products.csv 	new file:   products.json 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   tailwind.config.ts 	new file:   tsconfig.json

---
+++ app/api/products/route.ts
@@ -0,0 +1,24 @@
+/**
+ * TODO: Prepare an endpoint to return a list of products
+ * The endpoint should return a pagination of 10 products per page
+ * The endpoint should accept a query parameter "page" to return the corresponding page
+ */
+import { NextRequest, NextResponse } from 'next/server';
+import { fetchProducts } from '@/lib/products';
+
+export async function GET(req: NextRequest) {
+  try {
+    const page = parseInt(req.nextUrl.searchParams.get("page") || '1', 10);
+    const showAlcohol = req.nextUrl.searchParams.get("showAlcohol") || "false";
+
+    if (isNaN(page) || page < 1) {
+      return NextResponse.json({ error: "Invalid page number" }, { status: 400 });
+    }
+
+    const products = fetchProducts(page, showAlcohol);
+    return NextResponse.json(products);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return NextResponse.json({ error: "Failed to fetch products" }, { status: 500 });
+  }
+}
---
+++ lib/products.ts
@@ -0,0 +1,78 @@
+
+import dataJson from '../products.json';
+import fs from 'fs';
+import path from 'path';
+
+export type Product = {
+  id: string;
+  name: string;
+  price: number;
+  currency: string;
+  quantity: number;
+  isAlcohol: boolean;
+};
+
+
+export function fetchProducts(page: any, showAlcohol: string): Array<Product> {
+    if(page === 0){
+      page = 1;
+    }
+    const pageNumber = page || 1;
+    const itemsPerPage = 10;
+  
+    const filePath = path.join(process.cwd(), 'products.csv');
+
+      const dataCSV = readCSV(filePath);
+      var combinedData = [...dataJson, ...dataCSV];
+      combinedData = sortById(combinedData)
+
+      if(showAlcohol == 'false'){
+        combinedData = filterAlkohol(combinedData);
+      }
+
+      const startIndex = (pageNumber - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      const products = combinedData.slice(startIndex, endIndex);
+  
+      return products;
+    
+  }
+
+  const readCSV = (filePath: string): Product[] => {
+    const csvContent = fs.readFileSync(filePath, 'utf8');
+    const lines = csvContent.trim().split('\n');
+    const headers = lines[0].split(',');
+  
+    const jsonArray: Product[] = lines.slice(1).map(line => {
+      const values = line.split(',').map(value => value.trim());
+
+      const product: Product = {
+        id: values[0],
+        name: values[1],
+        price: parseFloat(values[2]),
+        currency: values[3],
+        quantity: parseInt(values[4], 10),
+        isAlcohol: values[5] === '1' 
+      };
+
+      return product;
+    });
+    return jsonArray;
+  };
+
+  function sortById(arr: Array<Product>) {
+    return arr.sort((a, b) => a.id.localeCompare(b.id));
+  }
+  
+
+  function filterAlkohol(arr: Array<Product>){
+    let filteredData: Array<Product> = [];
+
+    arr.map(product => {
+      if(!product.isAlcohol){
+        filteredData.push(product)
+      }  
+    })
+
+    return filteredData;
+  }
